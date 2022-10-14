df76f44f6f825cab7846ff8ac4475be07c938abb
Kuang-Ming Chen <kuang-ming.chen@aerq.com>
2022-10-14 14:39:44 +0200

    add fp functionality to service

--- src/services/taskManager/index.ts
+++ src/services/taskManager/index.ts
@@ -2,59 +2,136 @@ import { TaskNotFoundException, HttpApiException } from 'exceptions'
 import TaskManagerRepository from './repository'
 import { Task } from './types'
 
+import { either, taskEither } from 'fp-ts'
+import { pipe } from 'fp-ts/function'
 class TaskManagerService {
     constructor(private readonly repository: TaskManagerRepository) {}
 
     async retrieveTasks(user: string): Promise<Task[]> {
-        try {
-            return await this.repository.getTaskList(user)
-        } catch (e) {
-            throw new HttpApiException(500, `retriveTask ${user} failed: ${e}`)
-        }
+        const getTaskList = taskEither.tryCatch<Error, Task[]>(
+            async () => await this.repository.getTaskList(user),
+            (err) => err as Error
+        )
+
+        return await getTaskList().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        throw new HttpApiException(500, `retriveTask ${user} failed: ${e}`)
+                    },
+                    (x) => x
+                )
+            )
+        )
     }
     async deleteTask(user: string, id: string): Promise<void> {
-        try {
-            const task = await this.repository.getTask(user, id)
-            if (!task) {
-                throw new TaskNotFoundException()
-            }
-            await this.repository.removeTask(user, id)
-        } catch (e) {
-            if (e instanceof TaskNotFoundException) {
-                throw new HttpApiException(404, `Task ${id} not found: ${e}`)
-            }
-            throw new HttpApiException(500, `deleteTask ${id} failed: ${e}`)
-        }
+        const getTask = taskEither.tryCatch<Error, Task>(
+            async () => {
+                const savedTask = await this.repository.getTask(user, id)
+                if (!savedTask) {
+                    throw new TaskNotFoundException()
+                }
+                return savedTask
+            },
+            (err) => err as Error
+        )
+
+        await getTask().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        if (e instanceof TaskNotFoundException) {
+                            throw new HttpApiException(404, `Task ${id} not found: ${e}`)
+                        }
+                        throw new HttpApiException(500, `getTask failed with id ${id}: ${e}`)
+                    },
+                    (x) => x
+                )
+            )
+        )
+
+        const removeTask = taskEither.tryCatch<Error, number>(
+            async () => await this.repository.removeTask(user, id),
+            (err) => err as Error
+        )
+
+        await removeTask().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        throw new HttpApiException(500, `delete task ${id} failed: ${e}`)
+                    },
+                    (x) => x
+                )
+            )
+        )
     }
 
     async updateTask(user: string, id: string, completed: boolean, important: boolean): Promise<void> {
-        try {
-            const savedTask = await this.repository.getTask(user, id)
-            if (!savedTask) {
-                throw new TaskNotFoundException()
-            }
-            const mergedTask = {
-                ...savedTask,
-                completed,
-                important,
-                lastEdit: new Date().getTime(),
-            }
-            await this.repository.saveTask(user, mergedTask)
-        } catch (e) {
-            if (e instanceof TaskNotFoundException) {
-                throw new HttpApiException(404, `Task ${id} not found: ${e}`)
-            }
-            throw new HttpApiException(500, `updateTask ${id} failed: ${e}`)
-        }
+        const getTask = taskEither.tryCatch<Error, Task>(
+            async () => {
+                const savedTask = await this.repository.getTask(user, id)
+                if (!savedTask) {
+                    throw new TaskNotFoundException()
+                }
+                return savedTask
+            },
+            (err) => err as Error
+        )
+
+        const mergedTask = await getTask().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        if (e instanceof TaskNotFoundException) {
+                            throw new HttpApiException(404, `Task ${id} not found: ${e}`)
+                        }
+                        throw new HttpApiException(500, `getTask failed with id ${id}: ${e}`)
+                    },
+                    (savedTask) => ({ ...savedTask, completed, important, lastEdit: new Date().getTime() })
+                )
+            )
+        )
+
+        const updateTask = taskEither.tryCatch<Error, number>(
+            async () => await this.repository.saveTask(user, mergedTask),
+            (err) => err as Error
+        )
+
+        await updateTask().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        throw new HttpApiException(500, `update task failed with id ${id}: ${e}`)
+                    },
+                    (x) => x
+                )
+            )
+        )
     }
 
     async createTask(user: string, task: Task): Promise<void> {
-        try {
-            const numOfSavedTask = await this.repository.saveTask(user, task)
-            if (!numOfSavedTask) throw new HttpApiException(500, `createTask failed:`)
-        } catch (e) {
-            throw new HttpApiException(500, `createTask failed: ${e}`)
-        }
+        const saveTask = taskEither.tryCatch<Error, number>(
+            async () => await this.repository.saveTask(user, task),
+            (err) => err as Error
+        )
+
+        await saveTask().then((e) =>
+            pipe(
+                e,
+                either.fold(
+                    (e) => {
+                        throw new HttpApiException(500, `createTask failed: ${e}`)
+                    },
+                    (x) => x
+                )
+            )
+        )
     }
 }
 
